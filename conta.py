a7c01f033df96a5bf0c9908239d95c0654dcd192
Ckeska <gabriel.c.cesario@gmail.com>
2025-04-04 17:30:38 -0300

    Tamo programando Tamo programando

---
+++ conta.py
@@ -0,0 +1,15 @@
+class ContaCorrente:
+    def __init__(self,cliente,saldo,limite=500):
+        self.saldo = saldo
+        self.cliente = cliente
+        self.limite = limite
+    pass
+class ContaPoupanca:
+    def __init__(self,cliente,saldo):
+        self.cliente = cliente
+        self.saldo = saldo
+        pass
+    pass
+class GerenciadeConta:
+    
+    pass
