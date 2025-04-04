a7b8dec9242756b0636afff394646d0a68660fa3
Ckeska <gabriel.c.cesario@gmail.com>
2025-04-04 17:06:12 -0300

    Add files via upload

---
+++ Python_Banco/pessoa.py
@@ -0,0 +1,14 @@
+#Vamos lá, temos que fazer esse código com uma forma mais limpa, colocar as funcões nos códigos auxiliares e só chamar depois.
+listanomes = [] #nao sei se precisa estar aqui fora
+
+class Pessoa:
+    def __init__(self,nome,cpf):
+        self.nome = nome
+        self.cpf = cpf
+        pass
+
+class Gerente(Pessoa):
+    pass
+
+class Cliente(Pessoa):
+    pass
